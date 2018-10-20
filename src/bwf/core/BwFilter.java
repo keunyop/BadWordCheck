e8f5f8779e8f247da56ac4b3cd3c82aaf164b6bd
keunyop <kylee1112@hotmail.com>
2018-10-20 17:32:41 +0900

    developing

--- src/bwf/core/BwFilter.java
+++ src/bwf/core/BwFilter.java
@@ -1,9 +1,11 @@
 package bwf.core;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+
+import org.junit.platform.commons.util.StringUtils;
 
 public class BwFilter {
 	
@@ -26,23 +28,78 @@ public class BwFilter {
 		return results.toString();
 	}
 	
-	// Recursive로 둘글자 이상이면 재귀
-	public List<String> _makePatternList(String fWord) {
+	// Recursive로 두글자 이상이면 재귀
+	public Set<String> _makePatternSet(String fWord, String prefix, String postfix) {
 		
 		List<String> patternList = new ArrayList<>();
-		patternList.add(fWord);
 		
-		for(int i=0; i<fWord.length(); i++) {
-			for (char c : fWord.toCharArray()) {
+		Set<String> patternSet = new HashSet<>();
+		
+		if (prefix == null && postfix == null) {
+//			patternList.add(fWord);
+			patternSet.add(fWord);
+		}
+
+		if (fWord.length() > 1) {
+			for(int i=1; i<fWord.length(); i++) {
+				StringBuilder patternBuilder = new StringBuilder();
+				if (!StringUtils.isBlank(prefix)) {
+					patternBuilder.append(prefix);	
+				}
 				
+				int cnt=1;
+				for (char c : fWord.toCharArray()) {
+					patternBuilder.append(c);
+					
+					if (cnt==i) {
+						patternBuilder.append(" ");
+					}
+					
+					cnt++;
+				}
+				
+				if (!StringUtils.isBlank(postfix)) {
+					patternBuilder.append(postfix);
+				}
+				
+				String pattern = patternBuilder.toString();
+				
+//				patternList.add(pattern);
+				patternSet.add(pattern);
+				
+				String[] wordGroup = pattern.split(" ");
+				
+				for (int j=0; j<wordGroup.length; j++) {
+					StringBuilder prefixBuilder = new StringBuilder();
+					if (j!=0) {
+						for (int k=0; k<j; k++) {
+							prefixBuilder.append(wordGroup[k]);
+							prefixBuilder.append(" ");
+						}
+					}
+					
+					StringBuilder postfixBuilder = new StringBuilder();
+					if (j != wordGroup.length-1) {
+						for (int k=j+1; k<=wordGroup.length-1; k++) {
+							postfixBuilder.append(" ");
+							postfixBuilder.append(wordGroup[k]);
+						}
+					}
+					
+//						System.out.println("### prefixBuilder: " + prefixBuilder.toString());
+//						System.out.println("### postfixBuilder: " + postfixBuilder.toString());
+					
+//					patternList.addAll(_makePatternList(wordGroup[j], prefixBuilder.toString(), postfixBuilder.toString()));
+					patternSet.addAll(_makePatternSet(wordGroup[j], prefixBuilder.toString(), postfixBuilder.toString()));
+				}
 			}
 		}
+
 		
+		for (String string : patternSet) {
+			System.out.println(string);
+		}
 		
-		
-		
-		
-		
-		return patternList;
+		return patternSet;
 	}
 }
--- src/bwf/test/BwFilterTest.java
+++ src/bwf/test/BwFilterTest.java
@@ -13,6 +13,6 @@ class BwFilterTest {
 		BwFilter bwf = new BwFilter();
 //		assertEquals("", bwf.scan("가나다라금지어마바사가나다라금 지어마바사가나다라금지 어마바사가나다라금 지 어마바사"));
 		
-		assertEquals("금지어|금지 어|금 지어|금 지 어", bwf._makePattern("금지어"));
+		assertEquals("금지어|금지 어|금 지어|금 지 어", bwf._makePatternSet("금지어랑께", null, null));
 	}
 }
